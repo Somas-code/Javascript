550849ceb418f26c3c24b2598c10a9f1ba5035c5
Somas-code <somadinanwagbo@gmail.com>
2024-07-27 06:32:55 +0100

    strings & veriables

---
+++ app.js
@@ -0,0 +1,126 @@
+
+
+// exercise 1
+let name=("Somadina")
+let age=18
+let isstudent=true
+function bring(){
+alert(isstudent)
+}
+console.log(isstudent)
+
+
+// exercise 2
+let a = 5
+let b = 10
+let c = a
+a = b
+b = c
+console.log('a ='+ a)
+console.log('b ='+ b)
+
+
+// execrcise 3
+let x=10
+let y=5
+let z=x+y
+console.log(z)
+
+
+function difference() {
+    let z=x-y
+   console.log(z)
+}
+difference();
+
+
+function quotient() {
+    let z=x/y
+   console.log(z)
+}
+quotient();
+
+
+function product() {
+    let z=x*y
+   console.log(z)
+}
+product();
+
+
+// execrcise 4
+let greeting="hello"
+let subject="world"
+let p=greeting+" "+subject
+console.log(p)
+
+
+// execrcise 5
+let temperature= "50"
+if (temperature > "30") {
+     temperature="hot"
+} else {
+     temperature="warm"
+} 
+console.log(temperature)
+
+// function warm() {
+//     if (temperature <="30" & "20" ) {
+//         temperature="warm"
+//    } else {
+//         temperature="cold"
+//    } 
+//    console.log(warm) 
+// }
+
+// function cold() {
+//     if (temperature <"20" ) {
+//         temperature="cold"
+//    } else {
+//         temperature="cold"
+//    } 
+//    console.log(cold) 
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let red = 2
+// let blue = 5
+// let result=red+blue
+// console.log(result)
